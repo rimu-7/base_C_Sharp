80271dc97f76f382261eff9ef0d76513697e9699
rimu-7 <rrimu99@gmail.com>
2024-12-24 19:40:19 +0800

    Encapsulation, Inheritance, Abstraction

--- Solid/obj/Debug/net8.0/Solid.AssemblyInfo.cs
+++ Solid/obj/Debug/net8.0/Solid.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Solid")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528a43e7e0403f6f89d532c6e182a45bcc4ec7bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf43b6f2e7f9ab4a242459a2036fb664d3f41f14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Solid")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Solid")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
